4062016e94dbf62d778974ed951d4d182f216a46
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-19 16:52:42 +1300

    feat: Created REST API for getStudent, updateStudent and createAccount

---
+++ backend/src/main/java/com/nzpmc/demo/controllers/StudentController.java
@@ -0,0 +1,53 @@
+package com.nzpmc.demo.controllers;
+
+import com.nzpmc.demo.dto.student.StudentProfileDTO;
+import com.nzpmc.demo.dto.student.StudentRegistrationDTO;
+import com.nzpmc.demo.models.Student;
+import com.nzpmc.demo.services.StudentService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@CrossOrigin
+@RestController
+@RequestMapping("student")
+@AllArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping("{id}")
+    public ResponseEntity getStudent(@PathVariable String id) {
+        try {
+            StudentProfileDTO student = studentService.getStudentById(id);
+            return ResponseEntity.ok(student);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student with id " + id + " not found.");
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity updateStudent(@PathVariable String id, @Valid @RequestBody StudentProfileDTO student) {
+        try {
+            Student updatedStudent = studentService.updateStudentDetails(id, student);
+            return ResponseEntity.ok(updatedStudent);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student with id " + id + " not found.");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity createAccount(@Valid @RequestBody StudentRegistrationDTO studentRegistrationDTO) {
+        try {
+            studentService.createStudent(studentRegistrationDTO);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
+    }
+}
