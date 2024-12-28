99f84e325278ddca5671f51341c8e49bbd5b47e3
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-29 11:32:51 +1300

    feat: Created api for editing questions

--- backend/src/main/java/com/nzpmc/demo/controllers/admin/AdminQuestionController.java
+++ backend/src/main/java/com/nzpmc/demo/controllers/admin/AdminQuestionController.java
@@ -2,6 +2,7 @@ package com.nzpmc.demo.controllers.admin;
 
 import com.nzpmc.demo.dto.event.EventDetailDTO;
 import com.nzpmc.demo.dto.question.QuestionDTO;
+import com.nzpmc.demo.models.Event;
 import com.nzpmc.demo.models.Question;
 import com.nzpmc.demo.services.EventService;
 import com.nzpmc.demo.services.QuestionService;
@@ -10,6 +11,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @CrossOrigin
 @RestController
@@ -23,9 +26,19 @@ public class AdminQuestionController {
         try {
             Question createdQuestion = questionService.createQuestion(questionDTO);
             return ResponseEntity.status(HttpStatus.CREATED).body(createdQuestion);
-//            return ResponseEntity.ok().build();
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity updateQuestion(@PathVariable String id, @Valid @RequestBody QuestionDTO questionDTO) {
+        try {
+            Question updatedQuestion = questionService.editQuestion(id, questionDTO);
+            return ResponseEntity.ok(updatedQuestion);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Question with id " + id + " not found.");
+        }
+    }
+
 }
--- backend/src/main/java/com/nzpmc/demo/services/QuestionService.java
+++ backend/src/main/java/com/nzpmc/demo/services/QuestionService.java
@@ -1,11 +1,16 @@
 package com.nzpmc.demo.services;
 
+import com.nzpmc.demo.dto.event.EventDetailDTO;
 import com.nzpmc.demo.dto.question.QuestionDTO;
+import com.nzpmc.demo.mapper.event.EventDetailMapper;
 import com.nzpmc.demo.mapper.question.QuestionMapper;
+import com.nzpmc.demo.models.Event;
 import com.nzpmc.demo.models.Question;
 import com.nzpmc.demo.repository.QuestionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @AllArgsConstructor
@@ -14,6 +19,14 @@ public class QuestionService {
 
     public Question createQuestion(QuestionDTO questionDTO) {
         Question question = new QuestionMapper().convertToModel(questionDTO);
+        questionRepository.save(question);
+        return question;
+    }
+
+    public Question editQuestion(String id, QuestionDTO questionDTO) {
+        questionRepository.findById(id).orElseThrow(()-> new NoSuchElementException("Could not find question with id"));
+        Question question = new QuestionMapper().convertToModel(questionDTO);
+        question.setId(id);
         questionRepository.save(question);
         return question;
     }
