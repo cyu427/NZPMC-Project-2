a933da28b12843c8f8ac08cd8e6b5a4d34c7d9c3
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-29 11:08:25 +1300

    feat: Created api for creating questions

--- backend/src/main/java/com/nzpmc/demo/dto/question/QuestionDTO.java
+++ backend/src/main/java/com/nzpmc/demo/dto/question/QuestionDTO.java
@@ -1,12 +1,20 @@
 package com.nzpmc.demo.dto.question;
 
 import com.nzpmc.demo.models.Option;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class QuestionDTO {
+    @NotNull(message = "Question name is required")
     private String question;
+
+    @NotNull(message = "Options are required")
+    @Size(min = 4, max = 4, message = "There must be exactly 4 options")
+    @Valid
     private List<Option> options;
 }
--- backend/src/main/java/com/nzpmc/demo/models/Option.java
+++ backend/src/main/java/com/nzpmc/demo/models/Option.java
@@ -1,9 +1,13 @@
 package com.nzpmc.demo.models;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class Option {
+    @NotNull(message = "Option must have answer")
     private String text;
-    private boolean isCorrect;
+
+    @NotNull(message = "Option must be either true or false. It cannot be null")
+    private Boolean isCorrect;
 }
