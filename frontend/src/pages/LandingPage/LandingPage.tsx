aa70b08316fe603cefcf4ba18484b9a1d0f91a86
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-13 12:44:38 +1300

    style: Remove redundant comment

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,18 +1,3 @@
-// import Navigation from "../../components/navigation/Navigation";
-
-// const LandingPage: React.FC = () => {
-//     return (
-//         <div style={{width: '1200px', height: '100vh'}}>
-//             <div style={{position: 'sticky', top: 0}}>
-//                 <Navigation />
-//             </div>
-            
-//         </div>
-//     );
-// };
-  
-// export default LandingPage;
-
 import Navigation from "../../components/navigation/Navigation";
 
 const LandingPage: React.FC = () => {
