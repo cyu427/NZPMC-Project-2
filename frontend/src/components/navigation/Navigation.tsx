393b20407fdc29baa0f7b648f3217dbeca86da73
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-13 12:36:11 +1300

    refactor: Refactor all styling to have tailwind css

--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -1,11 +1,56 @@
+// import { Button, Typography } from "@mui/material";
+// import React, { useState } from "react";
+
+// const Navigation: React.FC = () => {
+//     const [ loggedin, setLoggedin ] = useState(false);
+
+//     const notLoggedInButtons = (
+//         <div style={{ display: 'flex', gap: '20px' }}>
+//             <Button variant="outlined" size="medium">
+//                 Register
+//             </Button>
+
+//             <Button variant="contained" size="medium">
+//                 Sign in
+//             </Button>
+//         </div>
+//     )
+
+//     const loggedInButtons = (
+//         <div>
+//             <Button variant="contained" size="medium">
+//                 Sign out
+//             </Button>
+//         </div>
+//     )
+
+//     return (
+//         <div style={{ display: 'flex', width: '1200px', justifyContent: 'space-between', alignItems: 'center'}}>
+//             <Typography variant="h6" component="div" sx={{ color: 'primary.main' }}>
+//                 NZPMC
+//             </Typography>
+
+//             <div>
+//                 { loggedin ? loggedInButtons : notLoggedInButtons }
+//             </div>
+
+
+//         </div>
+
+
+//     );
+// };
+
+// export default Navigation;
+
 import { Button, Typography } from "@mui/material";
 import React, { useState } from "react";
 
 const Navigation: React.FC = () => {
-    const [ loggedin, setLoggedin ] = useState(false);
+    const [loggedin, setLoggedin] = useState(false);
 
     const notLoggedInButtons = (
-        <div style={{ display: 'flex', gap: '20px' }}>
+        <div className="flex gap-5">
             <Button variant="outlined" size="medium">
                 Register
             </Button>
@@ -24,10 +69,8 @@ const Navigation: React.FC = () => {
         </div>
     )
 
-
-
     return (
-        <div style={{ display: 'flex', width: '1200px', justifyContent: 'space-between', alignItems: 'center'}}>
+        <div className="flex w-[1200px] justify-between items-center">
             <Typography variant="h6" component="div" sx={{ color: 'primary.main' }}>
                 NZPMC
             </Typography>
@@ -35,11 +78,7 @@ const Navigation: React.FC = () => {
             <div>
                 { loggedin ? loggedInButtons : notLoggedInButtons }
             </div>
-
-
         </div>
-
-
     );
 };
 
--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,26 @@
+// import Navigation from "../../components/navigation/Navigation";
+
+// const LandingPage: React.FC = () => {
+//     return (
+//         <div style={{width: '1200px', height: '100vh'}}>
+//             <div style={{position: 'sticky', top: 0}}>
+//                 <Navigation />
+//             </div>
+            
+//         </div>
+//     );
+// };
+  
+// export default LandingPage;
+
 import Navigation from "../../components/navigation/Navigation";
 
 const LandingPage: React.FC = () => {
     return (
-        <div style={{width: '1200px', height: '100vh'}}>
-            <div style={{position: 'sticky', top: 0}}>
+        <div className="w-[1200px] h-screen">
+            <div className="sticky top-0">
                 <Navigation />
             </div>
-            
         </div>
     );
 };
