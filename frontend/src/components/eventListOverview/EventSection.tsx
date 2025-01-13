a702326c43aa5de1eafd23905ebbafaa963f0174
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-13 17:53:32 +1300

    feat: Have the title and carousel together, and put them on the landing page

--- frontend/src/components/eventListOverview/EventSection.tsx
+++ frontend/src/components/eventListOverview/EventSection.tsx
@@ -1,30 +1,25 @@
 import { Typography } from "@mui/material";
-
-interface Event {
-    id: string;
-    name: string;
-    dateTime: Date;
-    location: string;
-    cost: string;
-    description: string;
-}
+import EventOverviewType from "./eventCard/utils/EventOverviewType";
+import EventCardModes from "./eventCard/utils/EventCardModes";
+import EventCarousel from "./eventCarousel/EventCarousel";
 
 interface EventSectionProps {
     title: string;
-    events: Event[];
+    events: EventOverviewType[];
+    mode: EventCardModes;
 }
 
-const EventSection: React.FC<EventSectionProps> = ({ title, events }) => {
+const EventSection: React.FC<EventSectionProps> = ({ title, events, mode }) => {
     return (
         <div>
             <div>
-                <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: 'text.primary' }}>
+                <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', color: 'text.primary', my: 2 }}>
                     {title}
                 </Typography>
             </div>
 
             <div>
-                
+                <EventCarousel events={events} mode={mode} />
             </div>
         </div>
     );
