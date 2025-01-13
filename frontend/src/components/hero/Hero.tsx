088c2cc5a559e4ec8efc9a2bbae962088a185cf2
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-14 01:36:53 +1300

    feat: Use the registration dialog in hero

--- frontend/src/components/hero/Hero.tsx
+++ frontend/src/components/hero/Hero.tsx
@@ -1,6 +1,8 @@
 import { Button, Dialog, styled, Typography } from "@mui/material";
 import { useState } from "react";
 import SignInDialog from "../auth/signin/SignInDialog";
+import RegisterDialog from "../auth/register/dialog/RegisterDialog";
+import RegisterProvider from "../../states/register/RegisterProvider";
 
 const Hero: React.FC = () => {
 
@@ -9,9 +11,13 @@ const Hero: React.FC = () => {
     })
 
     const [openSignInDialog, setOpenSignInDialog] = useState(false);
+    const [openRegisterDialog, setOpenRegisterDialog] = useState(false);
         
-        const handleSignIn = () => {setOpenSignInDialog(true);}
-        const handleCloseSignInDialog = () => {setOpenSignInDialog(false);}
+    const handleSignIn = () => {setOpenSignInDialog(true);}
+    const handleCloseSignInDialog = () => {setOpenSignInDialog(false);}
+
+    const handleRegister = () => {setOpenRegisterDialog(true);}
+    const handleCloseRegisterDialog = () => {setOpenRegisterDialog(false);}
 
     return (
         <>
@@ -26,7 +32,7 @@ const Hero: React.FC = () => {
                     </p>
 
                     <div> 
-                        <Button variant="outlined" sx={{width: '120px', height: '70px', marginRight: '150px', color: '#1864c4', backgroundColor: 'white'}}>
+                        <Button variant="outlined" onClick={handleRegister} sx={{width: '120px', height: '70px', marginRight: '150px', color: '#1864c4', backgroundColor: 'white'}}>
                             Register
                         </Button>
                         <Button variant="contained" onClick={handleSignIn} sx={{width: '120px', height: '70px'}}>
@@ -38,6 +44,12 @@ const Hero: React.FC = () => {
 
             <Dialog open={openSignInDialog} onClose={handleCloseSignInDialog} fullWidth maxWidth="sm">
                 <SignInDialog onClose={handleCloseSignInDialog} />
+            </Dialog>
+
+            <Dialog open={openRegisterDialog} onClose={handleCloseRegisterDialog} fullWidth maxWidth="sm">
+                <RegisterProvider>
+                    <RegisterDialog onClose={handleCloseRegisterDialog} />
+                </RegisterProvider>
             </Dialog>
 
         </>
