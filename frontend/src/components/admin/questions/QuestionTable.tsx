8e9dc796a17f5d29eb8964d250590df7b0e3d8b1
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-15 12:36:19 +1300

    feat: Put the search bar and table together

---
+++ frontend/src/components/admin/questions/QuestionTable.tsx
@@ -0,0 +1,64 @@
+import { Button } from "@mui/material";
+import { GridRenderCellParams } from "@mui/x-data-grid";
+import AdminDataTable from "../utils/AdminDataTable";
+import { useGetAllQuestions } from "../../../services/questions/useGetAllQuestions";
+import { useState } from "react";
+import QuestionOverviewType from "./QuestionOverviewType";
+import SearchBar from "../utils/SearchBar";
+
+const QuestionTable: React.FC = () => {
+    const columns = [
+        { field: 'title', headerName: 'Questions', width: 950 },
+        {
+            field: 'details',
+            headerName: 'View',
+            width: 125,
+            renderCell: (params: GridRenderCellParams) => (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => handleView(params.row)}
+              >
+                View
+              </Button>
+            ),
+        },
+        {
+            field: 'delete',
+            headerName: 'Delete',
+            width: 125,
+            renderCell: (params: GridRenderCellParams) => (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => handleDelete()}
+                sx = {{backgroundColor: 'red'}}
+              >
+                Delete
+              </Button>
+            ),
+        },
+
+    ];
+
+    const { data: allQuestions } = useGetAllQuestions();
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+
+    // Filter events based on search term
+    const filteredQuestions = allQuestions?.filter((question: QuestionOverviewType) =>
+        question.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <>
+            <SearchBar label="Search by Question name" value={searchTerm} onChange={handleSearchChange} />
+            <AdminDataTable columns={columns} rows={filteredQuestions} height={630} width={1200}/>
+        </>
+    );
+};
+
+export default QuestionTable;
