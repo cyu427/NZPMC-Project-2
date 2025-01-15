51140d1263db903929f41484c6a25cd54a80b146
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-15 17:16:51 +1300

    feat: added the dialog to competition page

--- frontend/src/pages/Admin/ChildPages/AdminCompetitionPage.tsx
+++ frontend/src/pages/Admin/ChildPages/AdminCompetitionPage.tsx
@@ -1,10 +1,15 @@
-import { Button, Typography } from "@mui/material";
-import React from "react";
+import { Button, Dialog, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { useGetAllCompetition } from "../../../services/competition/useGetAllCompetition";
 import CompetitionTable from "../../../components/admin/competition/CompetitionTable";
+import CreateCompetitionDialog from "../../../components/admin/competition/CreateCompetition/CreateCompetitionDialog";
 
 const AdminCompetitionPage: React.FC = () => {
     const { data: allCompetition, refetch: refetchAllCompetition } = useGetAllCompetition();
+
+    const [createCompetitionDialogOpen, setCreateCompetitionDialogOpen] = useState(false);
+    const handleCompetitionQuestion = () => {setCreateCompetitionDialogOpen(true);}
+    const handleCloseCreateCompetitionDialog = () => {setCreateCompetitionDialogOpen(false);}
 
     return (
         <>
@@ -12,13 +17,17 @@ const AdminCompetitionPage: React.FC = () => {
                 <Typography variant="h4" className="mb-4" sx={{ fontWeight: "bold" }}>
                     Competition
                 </Typography>
-                <Button variant="contained" size="medium" >
+                <Button variant="contained" size="medium" onClick={handleCompetitionQuestion}>
                     Create Competition
                 </Button>
             </div>
             <div className="justify-items-center">
                 <CompetitionTable allCompetition={allCompetition} refetchAllCompetition={refetchAllCompetition}  />
             </div>
+
+            <Dialog open={createCompetitionDialogOpen} onClose={handleCloseCreateCompetitionDialog} fullWidth maxWidth="md">
+                <CreateCompetitionDialog onClose={handleCloseCreateCompetitionDialog} refetchAllCompetitions={refetchAllCompetition} /> 
+            </Dialog>
         </>
     );
 };
