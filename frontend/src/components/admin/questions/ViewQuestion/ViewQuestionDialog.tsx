e67d5d6343bdd39682e668ebc902bb0ca7dd5e4e
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-15 15:58:28 +1300

    feat: Creat dialog to view question

---
+++ frontend/src/components/admin/questions/ViewQuestion/ViewQuestionDialog.tsx
@@ -0,0 +1,33 @@
+import { Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import { useGetQuestion } from "../../../../services/questions/useGetQuestion";
+import ViewQuestionDialogContent from "./ViewQuestionDialogContent";
+
+interface ViewQuestionDialogProps {
+    onClose: () => void;
+    id: string;
+}
+
+const ViewQuestionDialog: React.FC<ViewQuestionDialogProps> = ({ onClose, id }) => {
+    const { data: questionData, isLoading, isError } = useGetQuestion(id);
+    
+
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error...</div>;
+
+    return (
+        <Dialog open={true} onClose={onClose} maxWidth="sm" fullWidth>
+            <DialogTitle>
+                {questionData.question}
+                <IconButton onClick={onClose} aria-label="close" sx={{ position: 'absolute', right: 8, top: 8, color: (theme) => theme.palette.grey[500]}}> 
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContent>
+                <ViewQuestionDialogContent id={id} questionData={questionData} />
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default ViewQuestionDialog;
