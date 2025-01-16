58742fdba2be3ec5954d0f8b49fdc6b004ed56e5
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-16 17:37:58 +1300

    feat: Completed mapping of dto and question accounting the tags

--- backend/src/main/java/com/nzpmc/demo/mapper/question/QuestionMapper.java
+++ backend/src/main/java/com/nzpmc/demo/mapper/question/QuestionMapper.java
@@ -2,12 +2,42 @@ package com.nzpmc.demo.mapper.question;
 
 import com.nzpmc.demo.dto.question.QuestionDTO;
 import com.nzpmc.demo.models.Question;
+import com.nzpmc.demo.models.enums.Difficulty;
+import com.nzpmc.demo.models.enums.Topic;
 
 public class QuestionMapper {
     public Question convertToModel(QuestionDTO questionDTO) {
         Question question = new Question();
         question.setTitle(questionDTO.getQuestion());
         question.setOptions(questionDTO.getOptions());
+
+        switch(questionDTO.getDifficulty()) {
+            case "MEDIUM":
+                question.setDifficulty(Difficulty.MEDIUM);
+                break;
+            case "HARD":
+                question.setDifficulty(Difficulty.HARD);
+                break;
+            default:
+                question.setDifficulty(Difficulty.EASY);
+                break;
+        }
+
+        switch(questionDTO.getTopic()) {
+            case "WAVES":
+                question.setTopic(Topic.WAVES);
+                break;
+            case "ALGEBRA":
+                question.setTopic(Topic.ALGEBRA);
+                break;
+            case "GEOMETRY":
+                question.setTopic(Topic.GEOMETRY);
+                break;
+            default:
+                question.setTopic(Topic.MECHANICS);
+                break;
+        }
+
         return question;
     }
 
@@ -15,6 +45,8 @@ public class QuestionMapper {
         QuestionDTO questionDTO = new QuestionDTO();
         questionDTO.setQuestion(question.getTitle());
         questionDTO.setOptions(question.getOptions());
+        questionDTO.setDifficulty(String.valueOf(question.getDifficulty()));
+        questionDTO.setTopic(String.valueOf(question.getTopic()));
         return questionDTO;
     }
 }
--- backend/src/main/java/com/nzpmc/demo/projection/ViewAllQuestionProjection.java
+++ backend/src/main/java/com/nzpmc/demo/projection/ViewAllQuestionProjection.java
@@ -1,6 +1,11 @@
 package com.nzpmc.demo.projection;
 
+import com.nzpmc.demo.models.enums.Difficulty;
+import com.nzpmc.demo.models.enums.Topic;
+
 public interface ViewAllQuestionProjection {
     String getId();
     String getTitle();
+    Difficulty getDifficulty();
+    Topic getTopic();
 }
